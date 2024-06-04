5d0f7eb6d11e8c8cc08121e00419bf008e4a0ccb
MitarashiDango <mitarashi_dango@mail.matcha-soft.com>
2024-06-05 04:11:21 +0900

    XML出力用処理を追加

--- resource.go
+++ resource.go
@@ -2,21 +2,25 @@ package webfinger
 
 import (
 	"encoding/xml"
+	"fmt"
+	"slices"
 
 	"github.com/MitarashiDango/ohagi-go-webfinger/nullable"
 )
 
+type Properties map[string]nullable.String
+
 type Resource struct {
-	Subject    string   `json:"subject" xml:"Subject"`
-	Aliases    []string `json:"aliases" xml:"Alias"`
-	Properties map[string]nullable.String
-	Links      []*Link `json:"links" xml:"Link"`
+	Subject    string     `json:"subject"`
+	Aliases    []string   `json:"aliases,omitempty"`
+	Properties Properties `json:"properties,omitempty"`
+	Links      []Link     `json:"links,omitempty"`
 }
 
 type Link struct {
-	Rel  string `json:"rel" xml:"rel,attr"`
-	Type string `json:"type" xml:"type,attr"`
-	Href string `json:"href" xml:"href,attr"`
+	Rel  string `json:"rel,omitempty" xml:"rel,attr,omitempty"`
+	Type string `json:"type,omitempty" xml:"type,attr,omitempty"`
+	Href string `json:"href,omitempty" xml:"href,attr,omitempty"`
 }
 
 func (r *Resource) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
@@ -29,7 +33,7 @@ func (r *Resource) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 			Nullable bool   `xml:"nillable,attr"`
 			Value    string `xml:",chardata"`
 		} `xml:"Property"`
-		Links []*Link `xml:"Link"`
+		Links []Link `xml:"Link"`
 	}
 
 	if err := d.DecodeElement(&src, &start); err != nil {
@@ -53,5 +57,56 @@ func (r *Resource) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 }
 
 func (r Resource) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
-	return nil
+	type tmpProperties struct {
+		Type  string `xml:"type,attr,omitempty"`
+		Nil   bool   `xml:"xsi:nil,attr,omitempty"`
+		Value string `xml:",chardata"`
+	}
+
+	fmt.Println(start)
+	start.Name.Local = "XRD"
+	start.Name.Space = "http://docs.oasis-open.org/ns/xri/xrd-1.0"
+	start.Attr = append(start.Attr, xml.Attr{
+		Name: xml.Name{
+			Local: "xmlns:xsi",
+		},
+		Value: "http://www.w3.org/2001/XMLSchema-instance",
+	})
+
+	var src struct {
+		Subject    string          `xml:"Subject"`
+		Aliases    []string        `xml:"Alias,omitempty"`
+		Properties []tmpProperties `xml:"Property,omitempty"`
+		Links      []Link          `xml:"Link,omitempty"`
+	}
+
+	src.Subject = r.Subject
+	src.Aliases = r.Aliases
+	src.Links = r.Links
+
+	mapKeys := make([]string, 0, len(r.Properties))
+	for k := range r.Properties {
+		mapKeys = append(mapKeys, k)
+	}
+	slices.Sort(mapKeys)
+
+	src.Properties = make([]tmpProperties, 0, len(mapKeys))
+	for _, k := range mapKeys {
+		v := r.Properties[k]
+
+		if v.IsZero() {
+			src.Properties = append(src.Properties, tmpProperties{
+				Type: k,
+				Nil:  true,
+			})
+		} else {
+			src.Properties = append(src.Properties, tmpProperties{
+				Type:  k,
+				Nil:   false,
+				Value: v.StringOrZero(),
+			})
+		}
+	}
+
+	return e.EncodeElement(src, start)
 }
--- resource_test.go
+++ resource_test.go
@@ -1,13 +1,14 @@
 package webfinger
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"testing"
 
 	"github.com/MitarashiDango/ohagi-go-webfinger/nullable"
 )
 
-func Test_Resource_UnmarshalXML_000(t *testing.T) {
+func Test_Resource_UnmarshalXML_001(t *testing.T) {
 	xmlString := `<?xml version='1.0'?>
 <XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 <Subject>acct:test@localhost</Subject>
@@ -28,11 +29,11 @@ func Test_Resource_UnmarshalXML_000(t *testing.T) {
 	}
 
 	if dest.Subject != "acct:test@localhost" {
-		t.Fail()
+		t.FailNow()
 	}
 
 	if dest.Aliases[0] != "http://localhost/@test" {
-		t.Fail()
+		t.FailNow()
 	}
 
 	rProperties := map[string]bool{}
@@ -53,46 +54,217 @@ func Test_Resource_UnmarshalXML_000(t *testing.T) {
 	}
 
 	if v, ok := rProperties["testtype1"]; !ok || !v {
-		t.Fail()
+		t.FailNow()
 	}
 
 	if v, ok := rProperties["testtype2"]; !ok || !v {
-		t.Fail()
+		t.FailNow()
 	}
 
 	if v, ok := rProperties["testtype3"]; !ok || !v {
-		t.Fail()
+		t.FailNow()
 	}
 
 	if v, ok := rProperties["testtype4"]; !ok || !v {
-		t.Fail()
+		t.FailNow()
 	}
 
 	if dest.Links[0].Type != "text/html" {
-		t.Fail()
+		t.FailNow()
 	}
 
 	if dest.Links[0].Rel != "http://webfinger.net/rel/profile-page" {
-		t.Fail()
+		t.FailNow()
 	}
 
 	if dest.Links[0].Type != "text/html" {
-		t.Fail()
+		t.FailNow()
 	}
 
 	if dest.Links[0].Href != "http://localhost/@test" {
-		t.Fail()
+		t.FailNow()
 	}
 
 	if dest.Links[1].Rel != "self" {
-		t.Fail()
+		t.FailNow()
 	}
 
 	if dest.Links[1].Type != "application/activity+json" {
-		t.Fail()
+		t.FailNow()
 	}
 
 	if dest.Links[1].Href != "http://localhost/users/test" {
-		t.Fail()
+		t.FailNow()
+	}
+}
+
+func Test_Resource_UnmarshalJSON_001(t *testing.T) {
+	jsonString := `{"subject":"acct:test@localhost","aliases":["http://localhost/@test","http://localhost/users/test"],"properties":{"testtype1":"teststring1","testtype2":"teststring2","testtype3":null,"testtype4":null},"links":[{"rel":"http://webfinger.net/rel/profile-page","type":"text/html","href":"http://localhost/@test"},{"rel":"self","type":"application/activity+json","href":"http://localhost/users/test"}]}`
+
+	var dest Resource
+
+	if err := json.Unmarshal([]byte(jsonString), &dest); err != nil {
+		t.Fatal(err)
+	}
+
+	if dest.Subject != "acct:test@localhost" {
+		t.FailNow()
+	}
+
+	if dest.Aliases[0] != "http://localhost/@test" {
+		t.FailNow()
+	}
+
+	rProperties := map[string]bool{}
+	for k, actual := range dest.Properties {
+		var expected nullable.String
+		switch k {
+		case "testtype1":
+			expected.SetValue("teststring1")
+		case "testtype2":
+			expected.SetValue("teststring2")
+		case "testtype3":
+			expected.SetNil()
+		case "testtype4":
+			expected.SetNil()
+		}
+
+		rProperties[k] = expected.Equal(actual)
+	}
+
+	if v, ok := rProperties["testtype1"]; !ok || !v {
+		t.FailNow()
+	}
+
+	if v, ok := rProperties["testtype2"]; !ok || !v {
+		t.FailNow()
+	}
+
+	if v, ok := rProperties["testtype3"]; !ok || !v {
+		t.FailNow()
+	}
+
+	if v, ok := rProperties["testtype4"]; !ok || !v {
+		t.FailNow()
+	}
+
+	if dest.Links[0].Type != "text/html" {
+		t.FailNow()
+	}
+
+	if dest.Links[0].Rel != "http://webfinger.net/rel/profile-page" {
+		t.FailNow()
+	}
+
+	if dest.Links[0].Type != "text/html" {
+		t.FailNow()
+	}
+
+	if dest.Links[0].Href != "http://localhost/@test" {
+		t.FailNow()
+	}
+
+	if dest.Links[1].Rel != "self" {
+		t.FailNow()
+	}
+
+	if dest.Links[1].Type != "application/activity+json" {
+		t.FailNow()
+	}
+
+	if dest.Links[1].Href != "http://localhost/users/test" {
+		t.FailNow()
+	}
+}
+
+func Test_Resource_MarshalXML_001(t *testing.T) {
+	expected := `<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><Subject>acct:test@localhost</Subject><Alias>http://localhost/@test</Alias><Alias>http://localhost/users/test</Alias><Property type="testtype1">teststring1</Property><Property type="testtype2">teststring2</Property><Property type="testtype3" xsi:nil="true"></Property><Link rel="http://webfinger.net/rel/profile-page" type="text/html" href="http://localhost/@test"></Link><Link rel="self" type="application/activity+json" href="http://localhost/users/test"></Link></XRD>`
+	resource := &Resource{
+		Subject: "acct:test@localhost",
+		Aliases: []string{
+			"http://localhost/@test",
+			"http://localhost/users/test",
+		},
+		Properties: map[string]nullable.String{
+			"testtype1": {
+				Valid:  true,
+				String: "teststring1",
+			},
+			"testtype2": {
+				Valid:  true,
+				String: "teststring2",
+			},
+			"testtype3": {
+				Valid:  false,
+				String: "",
+			},
+		},
+		Links: []Link{
+			{
+				Rel:  "http://webfinger.net/rel/profile-page",
+				Type: "text/html",
+				Href: "http://localhost/@test",
+			},
+			{
+				Rel:  "self",
+				Type: "application/activity+json",
+				Href: "http://localhost/users/test",
+			},
+		},
+	}
+
+	b, err := xml.Marshal(resource)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(b) != expected {
+		t.FailNow()
+	}
+}
+
+func Test_Resource_MarshalJSON_001(t *testing.T) {
+	expected := `{"subject":"acct:test@localhost","aliases":["http://localhost/@test","http://localhost/users/test"],"properties":{"testtype1":"teststring1","testtype2":"teststring2","testtype3":null},"links":[{"rel":"http://webfinger.net/rel/profile-page","type":"text/html","href":"http://localhost/@test"},{"rel":"self","type":"application/activity+json","href":"http://localhost/users/test"}]}`
+	resource := &Resource{
+		Subject: "acct:test@localhost",
+		Aliases: []string{
+			"http://localhost/@test",
+			"http://localhost/users/test",
+		},
+		Properties: map[string]nullable.String{
+			"testtype1": {
+				Valid:  true,
+				String: "teststring1",
+			},
+			"testtype2": {
+				Valid:  true,
+				String: "teststring2",
+			},
+			"testtype3": {
+				Valid:  false,
+				String: "",
+			},
+		},
+		Links: []Link{
+			{
+				Rel:  "http://webfinger.net/rel/profile-page",
+				Type: "text/html",
+				Href: "http://localhost/@test",
+			},
+			{
+				Rel:  "self",
+				Type: "application/activity+json",
+				Href: "http://localhost/users/test",
+			},
+		},
+	}
+
+	b, err := json.Marshal(resource)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(b) != expected {
+		t.FailNow()
 	}
 }
