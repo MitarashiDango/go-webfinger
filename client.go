cd65b1b6a3c0778aeda393cd6e91c212732e2c77
MitarashiDango <mitarashi_dango@mail.matcha-soft.com>
2024-06-05 19:20:45 +0900

    WebFingerクライアント処理実装

---
+++ client.go
@@ -0,0 +1,129 @@
+package webfinger
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"io"
+	"mime"
+	"net/http"
+	"net/url"
+)
+
+var DefaultClient = &Client{
+	HTTPClient: &http.Client{},
+}
+
+type Client struct {
+	HTTPClient *http.Client
+	UserAgent  string
+	HTTPMode   bool
+}
+
+func (client *Client) Do(webFingerRequest *Request) (*Message, error) {
+	request, err := client.createHTTPRequest(client.HTTPMode, webFingerRequest)
+	if err != nil {
+		return nil, &Error{
+			Err: err,
+		}
+	}
+
+	response, err := client.HTTPClient.Do(request)
+	if err != nil {
+		return nil, &Error{
+			Err: err,
+		}
+	}
+	defer response.Body.Close()
+
+	if err := client.statusCodeToError(response); err != nil {
+		return nil, err
+	}
+
+	mimeType, _, err := mime.ParseMediaType(response.Header.Get("Content-Type"))
+	if err != nil {
+		return nil, &Error{
+			Err: err,
+		}
+	}
+
+	var webFingerMessage Message
+	switch {
+	case mimeType == "application/xrd+xml" || mimeType == "application/xml" || mimeType == "text/xml":
+		b, err := io.ReadAll(response.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		if err := xml.Unmarshal(b, &webFingerMessage); err != nil {
+			return nil, &Error{
+				Err: err,
+			}
+		}
+
+		return &webFingerMessage, nil
+
+	case mimeType == "application/jrd+json" || mimeType == "application/json":
+		b, err := io.ReadAll(response.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		if err := json.Unmarshal(b, &webFingerMessage); err != nil {
+			return nil, &Error{
+				Err: err,
+			}
+		}
+
+		return &webFingerMessage, nil
+
+	default:
+		return nil, &Error{
+			Err: &UnsupportedContentTypeError{
+				ContentType: mimeType,
+			},
+		}
+	}
+}
+
+func (client *Client) createHTTPRequest(httpMode bool, webFingerRequest *Request) (*http.Request, error) {
+	requestURL := getSchema(httpMode) + "//" + webFingerRequest.Host + "/.well-known/webfinger?resource=" + url.QueryEscape(webFingerRequest.Resource)
+	request, err := http.NewRequest("GET", requestURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header.Set("Accept", "application/jrd+json, application/xrd+xml")
+
+	if client.UserAgent != "" {
+		request.Header.Set("User-Agent", client.UserAgent)
+	}
+
+	return request, nil
+}
+
+func (client *Client) statusCodeToError(response *http.Response) error {
+	if response.StatusCode < 200 || response.StatusCode >= 300 {
+		switch {
+		case response.StatusCode == http.StatusNotFound || response.StatusCode == http.StatusGone:
+			return ErrResourceNotFound
+		default:
+			return &Error{
+				Err: &WebFingerResponseStatusError{
+					URL:        response.Request.URL.String(),
+					StatusCode: response.StatusCode,
+					Status:     response.Status,
+				},
+			}
+		}
+	}
+
+	return nil
+}
+
+func getSchema(httpMode bool) string {
+	if !httpMode {
+		return "https:"
+	}
+
+	return "http:"
+}
---
+++ client_test.go
@@ -0,0 +1,441 @@
+package webfinger_test
+
+import (
+	"errors"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	webfinger "github.com/MitarashiDango/ohagi-go-webfinger"
+)
+
+func Test_Client_Do_JSONResponse(t *testing.T) {
+
+	var baseURL string
+	var host string
+	renderWebFingerResponse := func() string {
+		return `
+		{
+			"subject": "acct:test@` + host + `",
+			"aliases": [
+					"` + baseURL + `/@test",
+					"` + baseURL + `/users/test"
+			],
+			"links": [
+					{
+							"rel": "self",
+							"type": "application/activity+json",
+							"href": "` + baseURL + `/users/test"
+					}
+			]
+	}
+		`
+	}
+
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch {
+		case r.URL.Path == "/.well-known/webfinger":
+			if r.URL.Query().Get("resource") != "acct:test@"+host {
+				t.Errorf("unexpected query value: %s: %s", "resource", r.URL.Query().Get("resource"))
+			}
+
+			w.Header().Set("Content-Type", "application/jrd+json")
+			io.WriteString(w, renderWebFingerResponse())
+		default:
+			t.Errorf("unexpected path: %s", r.URL.Path)
+		}
+	}))
+	defer testServer.Close()
+
+	baseURL = testServer.URL
+
+	u, err := url.Parse(testServer.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	host = u.Host
+
+	client := &webfinger.Client{
+		HTTPClient: http.DefaultClient,
+		HTTPMode:   true,
+	}
+
+	message, err := client.Do(&webfinger.Request{Host: host, Resource: "acct:test@" + host})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if message.Subject != "acct:test@"+host {
+		t.FailNow()
+	}
+}
+
+func Test_Client_Do_XMLResponse(t *testing.T) {
+
+	var baseURL string
+	var host string
+	renderWebFingerResponse := func() string {
+		return `<?xml version='1.0'?>
+		<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+		<Subject>acct:test@` + host + `</Subject>
+		<Alias>` + baseURL + `/@test</Alias>
+		<Alias>` + baseURL + `/users/test</Alias>
+		<Property type="testtype1">teststring1</Property>
+		<Property type="testtype2">teststring2</Property>
+		<Property type="testtype3" xsi:nil="true" />
+		<Property type="testtype4" nillable="true" />
+		<Link rel="http://webfinger.net/rel/profile-page" type="text/html" href="` + baseURL + `/@test"/>
+		<Link rel="self" type="application/activity+json" href="` + baseURL + `/users/test"/>
+		</XRD>`
+	}
+
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch {
+		case r.URL.Path == "/.well-known/webfinger":
+			if r.URL.Query().Get("resource") != "acct:test@"+host {
+				t.Errorf("unexpected query value: %s: %s", "resource", r.URL.Query().Get("resource"))
+			}
+
+			w.Header().Set("Content-Type", "application/xrd+xml")
+			io.WriteString(w, renderWebFingerResponse())
+		default:
+			t.Errorf("unexpected path: %s", r.URL.Path)
+		}
+	}))
+	defer testServer.Close()
+
+	baseURL = testServer.URL
+
+	u, err := url.Parse(testServer.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	host = u.Host
+
+	client := &webfinger.Client{
+		HTTPClient: http.DefaultClient,
+		HTTPMode:   true,
+	}
+
+	message, err := client.Do(&webfinger.Request{Host: host, Resource: "acct:test@" + host})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if message.Subject != "acct:test@"+host {
+		t.FailNow()
+	}
+}
+
+func Test_Client_Do_CustomUserAgent(t *testing.T) {
+
+	var baseURL string
+	var host string
+	renderWebFingerResponse := func() string {
+		return `
+		{
+			"subject": "acct:test@` + host + `",
+			"aliases": [
+					"` + baseURL + `/@test",
+					"` + baseURL + `/users/test"
+			],
+			"links": [
+					{
+							"rel": "self",
+							"type": "application/activity+json",
+							"href": "` + baseURL + `/users/test"
+					}
+			]
+	}
+		`
+	}
+
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch {
+		case r.URL.Path == "/.well-known/webfinger":
+			if r.URL.Query().Get("resource") != "acct:test@"+host {
+				t.Errorf("unexpected query value: %s: %s", "resource", r.URL.Query().Get("resource"))
+			}
+
+			if r.Header.Get("User-Agent") != "Test User Agent" {
+				t.Errorf("unexpected user agent: %s)", r.Header.Get("User-Agent"))
+			}
+
+			w.Header().Set("Content-Type", "application/jrd+json")
+			io.WriteString(w, renderWebFingerResponse())
+		default:
+			t.Errorf("unexpected path: %s", r.URL.Path)
+		}
+	}))
+	defer testServer.Close()
+
+	baseURL = testServer.URL
+
+	u, err := url.Parse(testServer.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	host = u.Host
+
+	client := &webfinger.Client{
+		HTTPClient: http.DefaultClient,
+		HTTPMode:   true,
+		UserAgent:  "Test User Agent",
+	}
+
+	message, err := client.Do(&webfinger.Request{Host: host, Resource: "acct:test@" + host})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if message.Subject != "acct:test@"+host {
+		t.FailNow()
+	}
+}
+
+func Test_Client_Do_NotFound(t *testing.T) {
+	var host string
+
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch {
+		case r.URL.Path == "/.well-known/webfinger":
+			w.WriteHeader(404)
+		default:
+			t.Errorf("unexpected path: %s", r.URL.Path)
+		}
+	}))
+	defer testServer.Close()
+
+	u, err := url.Parse(testServer.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	host = u.Host
+
+	client := &webfinger.Client{
+		HTTPClient: http.DefaultClient,
+		HTTPMode:   true,
+	}
+
+	message, err := client.Do(&webfinger.Request{Host: host, Resource: "acct:test@" + host})
+	if err != nil {
+		if !errors.Is(err, webfinger.ErrResourceNotFound) {
+			t.Error(err)
+		}
+		return
+	}
+
+	if message != nil {
+		t.FailNow()
+	}
+}
+
+func Test_Client_Do_Gone(t *testing.T) {
+	var host string
+
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch {
+		case r.URL.Path == "/.well-known/webfinger":
+			w.WriteHeader(410)
+		default:
+			t.Errorf("unexpected path: %s", r.URL.Path)
+		}
+	}))
+	defer testServer.Close()
+
+	u, err := url.Parse(testServer.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	host = u.Host
+
+	client := &webfinger.Client{
+		HTTPClient: http.DefaultClient,
+		HTTPMode:   true,
+	}
+
+	message, err := client.Do(&webfinger.Request{Host: host, Resource: "acct:test@" + host})
+	if err != nil {
+		if !errors.Is(err, webfinger.ErrResourceNotFound) {
+			t.Error(err)
+		}
+		return
+	}
+
+	if message != nil {
+		t.FailNow()
+	}
+}
+
+func Test_Client_Do_OtherHTTPStatusError(t *testing.T) {
+	var host string
+
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch {
+		case r.URL.Path == "/.well-known/webfinger":
+			w.WriteHeader(500)
+		default:
+			t.Errorf("unexpected path: %s", r.URL.Path)
+		}
+	}))
+	defer testServer.Close()
+
+	u, err := url.Parse(testServer.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	host = u.Host
+
+	client := &webfinger.Client{
+		HTTPClient: http.DefaultClient,
+		HTTPMode:   true,
+	}
+
+	message, err := client.Do(&webfinger.Request{Host: host, Resource: "acct:test@" + host})
+	if err != nil {
+		var webFingerError *webfinger.Error
+		var webFingerResponseStatusError *webfinger.WebFingerResponseStatusError
+		if !errors.As(err, &webFingerError) {
+			t.Error(err)
+		} else if !errors.As(webFingerError, &webFingerResponseStatusError) {
+			t.Error(err)
+		}
+		return
+	}
+
+	if message != nil {
+		t.FailNow()
+	}
+}
+
+func Test_Client_Do_UnsupportedContentType(t *testing.T) {
+	var host string
+
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch {
+		case r.URL.Path == "/.well-known/webfinger":
+			w.Header().Add("Content-Type", "text/plain")
+			w.Write([]byte("test"))
+		default:
+			t.Errorf("unexpected path: %s", r.URL.Path)
+		}
+	}))
+	defer testServer.Close()
+
+	u, err := url.Parse(testServer.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	host = u.Host
+
+	client := &webfinger.Client{
+		HTTPClient: http.DefaultClient,
+		HTTPMode:   true,
+	}
+
+	message, err := client.Do(&webfinger.Request{Host: host, Resource: "acct:test@" + host})
+	if err != nil {
+		var webFingerError *webfinger.Error
+		var unsupportedContentTypeError *webfinger.UnsupportedContentTypeError
+		if !errors.As(err, &webFingerError) {
+			t.Error(err)
+		} else if !errors.As(webFingerError, &unsupportedContentTypeError) {
+			t.Error(err)
+		}
+		return
+	}
+
+	if message != nil {
+		t.FailNow()
+	}
+}
+
+func Test_Client_Do_DoHTTPRequestFailed(t *testing.T) {
+	var host string
+
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch {
+		case r.URL.Path == "/.well-known/webfinger":
+			w.WriteHeader(410)
+		default:
+			t.Errorf("unexpected path: %s", r.URL.Path)
+		}
+	}))
+	defer testServer.Close()
+
+	u, err := url.Parse(testServer.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	host = u.Host
+
+	client := &webfinger.Client{
+		HTTPClient: http.DefaultClient,
+		HTTPMode:   true,
+	}
+
+	message, err := client.Do(&webfinger.Request{Host: "127.0.0.1.test", Resource: "acct:test@" + host})
+	if err != nil {
+		var webFingerError *webfinger.Error
+		if !errors.As(err, &webFingerError) {
+			t.Error(err)
+		}
+		return
+	}
+
+	if message != nil {
+		t.FailNow()
+	}
+}
+
+func Test_Client_Do_InvalidContentType(t *testing.T) {
+	var host string
+
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch {
+		case r.URL.Path == "/.well-known/webfinger":
+			w.Header().Add("Content-Type", "multipart/form-data; test=")
+		default:
+			t.Errorf("unexpected path: %s", r.URL.Path)
+		}
+	}))
+	defer testServer.Close()
+
+	u, err := url.Parse(testServer.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	host = u.Host
+
+	client := &webfinger.Client{
+		HTTPClient: http.DefaultClient,
+		HTTPMode:   true,
+	}
+
+	message, err := client.Do(&webfinger.Request{Host: "127.0.0.1.test", Resource: "acct:test@" + host})
+	if err != nil {
+		var webFingerError *webfinger.Error
+		if !errors.As(err, &webFingerError) {
+			t.Error(err)
+		}
+		return
+	}
+
+	if message != nil {
+		t.FailNow()
+	}
+}
